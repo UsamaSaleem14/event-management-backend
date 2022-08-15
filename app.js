b84a7a986d042501f6e1a2f23713c004074d3de3
UsamaSaleem14 <usamasaleem0148@gmail.com>
2022-08-15 13:18:45 +0500

    Added headers for incoming and outgoing requests

--- app.js
+++ app.js
@@ -1,21 +1,33 @@
-const express = require("express")
-const bodyParser = require("body-parser")
-const { graphqlHTTP } = require("express-graphql")
-const mongoose = require("mongoose")
-const isAuth = require("./middleware/is-auth")
+const express = require('express')
+const bodyParser = require('body-parser')
+const { graphqlHTTP } = require('express-graphql')
+const mongoose = require('mongoose')
+const isAuth = require('./middleware/is-auth')
 
 // GraphQl schema import
-const graphQlSchema = require("./graphql/schemas/index")
-const graphQlResolver = require("./graphql/resolvers/index")
+const graphQlSchema = require('./graphql/schemas/index')
+const graphQlResolver = require('./graphql/resolvers/index')
 
 const app = express()
 
 app.use(bodyParser.json())
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200)
+  }
+
+  next()
+})
+
 app.use(isAuth)
 
 app.use(
-  "/api",
+  '/api',
   graphqlHTTP({
     schema: graphQlSchema,
     rootValue: graphQlResolver,
@@ -26,7 +38,7 @@ app.use(
 mongoose
   .connect(`mongodb+srv://${process.env.MONGO_USER_ADMIN}:${process.env.MONGO_PASSWORD_ADMIN}@eventmanagementgraphql.ngqth.mongodb.net/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority`)
   .then(() => {
-    app.listen(3001)
+    app.listen(8000)
   })
   .catch((err) => {
     console.log(err)
